9aa8f8c623b966622671287151eae4f82b98abdb
Amogh Teli <teliamogh7578@gmail.com>
2026-01-15 10:32:48 +0530

    Set up smooth scrolling

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@ const yearElement = document.querySelector(".year");
 if (yearElement) {
   yearElement.textContent = new Date().getFullYear();
 }
-// MOBILE NAV TOGGLE
+// Mobile nav toggle
 const mobileNavButton = document.querySelector(".btn-mobile-nav");
 const siteHeader = document.querySelector(".header");
 
@@ -13,3 +13,26 @@ if (mobileNavButton && siteHeader) {
     siteHeader.classList.toggle("nav-open");
   });
 }
+// Smooth scrolling
+const anchorLinks = document.querySelectorAll("a[href^='#']");
+
+anchorLinks.forEach((link) => {
+  link.addEventListener("click", (event) => {
+    const targetId = link.getAttribute("href");
+    if (!targetId) return;
+    event.preventDefault();
+    if (targetId === "#") {
+      window.scrollTo({ top: 0, behavior: "smooth" });
+      return;
+    }
+
+    const targetSection = document.querySelector(targetId);
+    if (targetSection) {
+      targetSection.scrollIntoView({ behavior: "smooth" });
+    }
+
+    if (link.classList.contains("main-nav-link") && siteHeader) {
+      siteHeader.classList.remove("nav-open");
+    }
+  });
+});
